16549f30f63d85eb7bf752971e2b3e1ad0f5e3d0
Christopher Loisel <christopher@MacBook-Pro-de-Christopher.local>
2023-11-26 14:28:53 +0100

    form user et addmessage

--- src/app/Components/Threads/threads.component.ts
+++ src/app/Components/Threads/threads.component.ts
@@ -1,41 +1,59 @@
-import { Component, OnInit } from '@angular/core';
-import { CommonModule } from '@angular/common';
-import { Thread, ThreadsService } from '../../Services/threads.service';
-import { MessagesComponent } from '../Messages/messages.component';
-import { Message, MessagesService } from '../../Services/messages.service';
+import { Component, Input, OnInit } from "@angular/core";
+import { CommonModule } from "@angular/common";
+import { Thread, ThreadsService } from "../../Services/threads.service";
+import { MessagesComponent } from "../Messages/messages.component";
+import { Message, MessagesService } from "../../Services/messages.service";
+import { FormsModule } from "@angular/forms";
+import { UserService } from "../../Services/user.service";
 
 @Component({
-  selector: 'app-threads',
-  standalone: true,
-  imports: [CommonModule, MessagesComponent],
-  templateUrl: './threads.component.html',
-  styleUrl: './threads.component.css',
+    selector: "app-threads",
+    standalone: true,
+    imports: [CommonModule, MessagesComponent, FormsModule],
+    templateUrl: "./threads.component.html",
+    styleUrl: "./threads.component.css",
 })
 export class ThreadsComponent implements OnInit {
-  threads!: Thread[];
-  actualThread!: Thread;
-  messages!: Message[];
+    threads!: Thread[];
+    actualThread!: Thread;
+    messages!: Message[];
+    @Input()
+    message!: string;
 
-  constructor(
-    private threadsService: ThreadsService,
-    private messagesService: MessagesService
-  ) {}
+    constructor(
+        public threadsService: ThreadsService,
+        public messagesService: MessagesService,
+        public userService: UserService
+    ) {}
 
-  selectThread(thread: Thread) {
-    this.actualThread = thread;
-    this.messagesService
-      .getMessagesByThreadId(thread.id)
-      .subscribe((messages: any) => {
-        this.messages = messages;
-        console.log(this.messages);
-      });
-  }
-  // Observable
+    selectThread(thread: Thread) {
+        this.actualThread = thread;
+        this.messagesService
+            .getMessagesByThreadId(thread.id)
+            .subscribe((messages: any) => {
+                this.messages = messages;
+                console.log(this.messages);
+            });
+    }
 
-  ngOnInit() {
-    this.threadsService.getThreads().subscribe((threads: any) => {
-      this.threads = threads;
-      console.log(this.threads);
-    }); // Récupère les threads depuis le service
-  }
+    ngOnInit() {
+        this.threadsService.getThreads().subscribe((threads: any) => {
+            this.threads = threads;
+            console.log(this.threads);
+            this.selectThread(this.threads[0]);
+        });
+    }
+
+    sendMessage() {
+        this.messagesService
+            .createMessage({
+                content: this.message,
+                authorId: this.userService.user?.id,
+                threadId: this.actualThread.id,
+            })
+            .subscribe((message: any) => {
+                this.messages.push(message);
+                this.message = "";
+            });
+    }
 }
--- src/app/Services/user.service.ts
+++ src/app/Services/user.service.ts
@@ -1,19 +1,30 @@
-import { HttpClient } from '@angular/common/http';
-import { Injectable } from '@angular/core';
+import { HttpClient } from "@angular/common/http";
+import { Injectable } from "@angular/core";
+
+export interface User {
+    id: number;
+    username: string;
+}
 
 @Injectable({
-  providedIn: 'root',
+    providedIn: "root",
 })
 export class UserService {
-  User = {};
+    user: undefined | User;
 
-  constructor(private http: HttpClient) {}
+    constructor(private http: HttpClient) {}
 
-  login(user: any) {
-    return this.http.post('http://localhost:3000/login', user);
-  }
+    login(username: string) {
+        console.log("login" + username);
+        this.http
+            .post("http://localhost:3000/users", { username })
+            .subscribe((user: any) => {
+                console.log("user", user);
+                this.user = user;
+            });
+    }
 
-  logout() {
-    this.User = {};
-  }
+    logout() {
+        this.user = undefined;
+    }
 }
